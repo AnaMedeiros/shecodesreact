341c07d70ee909ba697fa36c4125d64cde1a6dd5
AnaMedeiros <anaghmedeiros@gmail.com>
2020-12-13 20:02:51 +0000

    import components into APP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Header";
+import Footer from "./Footer";
+import NextDays from "./NextDays";
+import Today from "./Today";
+import Tomorow from "./Tomorow";
+import "./index.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Header />
+
+        <div className="row forcastCard">
+          <Today />
+
+          <div className="col-md-6 rightSide">
+            <Tomorow />
+            <NextDays />
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
-import "./styles.css";
+import reactLogo from "./images/react_logo.png";
 
 export default function Header() {
   return (
     <div className="secondHeader">
       <img
         className="sheCodesReactLogo"
-        src="https://www.shecodes.io/assets/products/logos/react-bdcb55adddce8fd811946f5e2f6f32ec3116a79b8c503323b703802700f19131.png"
+        src={reactLogo}
         alt="SheCodesReactLogo"
       />
       <h1 className="header">Weather Forecast</h1>
